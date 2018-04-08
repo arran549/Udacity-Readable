93b8f1893c3104ab98226649a5b182983b94c7b3
Arran Dyer <arran549@gmail.com>
2018-04-08 22:07:04 +0100

    style:  move class to inner div

--- src/App.js
+++ src/App.js
@@ -21,9 +21,9 @@ class App extends Component {
     
 
     return (
-      <div className="App">
+      <div >
         <Route exact path="/" render={() => (
-          <div>
+          <div className="App">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <h1 className="App-title">Welcome to React</h1>
