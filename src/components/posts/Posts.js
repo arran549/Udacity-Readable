6b547bcb36aa3a4188d63dd76a65530aefa5e727
Arran Dyer <arran549@gmail.com>
2018-04-08 22:07:37 +0100

    feat: display posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,12 +1,47 @@
 import React from 'react'
 
+//import * as ReadableAPI from './../../readableAPI'
+import { getAllPosts } from './../../readableAPI'
+import { PageHeader, Panel, Button } from 'react-bootstrap'
+
 
 class Posts extends React.Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+
+        //let posts = getAllPosts();
+
+        //console.log(posts);
+
+        getAllPosts().then((posts) => {
+            console.log('posts', posts)
+            this.setState({ posts })
+        })
+    }
 
     render() {
 
         return (
-            <h1>Hello World!</h1>
+            <div>
+                <PageHeader>Posts</PageHeader>
+                <ol>
+                {  this.state.posts.map( (post) => ( 
+                    <Panel>
+                        <Panel.Heading key={post.id}>{post.title}</Panel.Heading>
+                        <Panel.Body>{post.body}
+                        <br />
+                        <Button>Vote Down</Button>
+                        <Button>Vote Up</Button>
+                        </Panel.Body>
+                        
+                    </Panel>
+                ))}
+                </ol>
+            </div>
         )
     }
 
