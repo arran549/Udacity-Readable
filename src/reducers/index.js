75e5917ec08977b853f2ffeee0f17ab6ce31e8f8
Arran Dyer <arran549@gmail.com>
2018-04-08 16:47:15 +0100

    chore: setup react-redux

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux'
+
+//import {
+//    ADD_RECIPE,
+//    REMOVE_FROM_CALENDAR
+//} from '../actions'
+
+const initialPostsState = {
+
+}
+
+function posts (state = initialPostsState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default combineReducers({ 
+    posts, 
+});
