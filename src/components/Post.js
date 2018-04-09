9d6746ebec59d62f76f650b651391c6cf8fb70ed
Arran Dyer <arran549@gmail.com>
2018-04-09 18:19:02 +0100

    feat: display author and timestamp on post

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,12 +3,16 @@ import { PageHeader, Panel, Button, Badge, Row, Col } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import VoteOnPost from './VoteOnPost'
 import PostComments from './PostComments'
+import Moment from 'react-moment'
+
 
 class Post extends Component {
     state = {  }
     render() {
 
         const { post } = this.props;
+
+        const timestamp = new Date(post.timestamp);
 
         return (
             <div key={post.id}>
@@ -17,7 +21,10 @@ class Post extends Component {
                     <Panel.Body>
                     <br />
                     <Row>
-                        <Col md={8}>{post.body}</Col>
+                        <Col md={8}>
+                            <p>{post.body}</p>
+                            <p><small>Written by: <b>{post.author}</b> | <i><Moment date={timestamp} format="ddd DD MMM @ HH:mm"></Moment></i> </small> </p>
+                        </Col>
                         <Col md={2}>
                             <PostComments post={post}></PostComments>
                         </Col>
