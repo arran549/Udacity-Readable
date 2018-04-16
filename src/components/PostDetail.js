91cb8e73df465111c3302aa8713d7b63f5d73837
Arran Dyer <arran549@gmail.com>
2018-04-16 10:03:20 +0100

    feat: display post detail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Post from './Post';
+import { Panel, Badge, Row, Col } from 'react-bootstrap'
+import VoteOnPost from './VoteOnPost'
+import PostComments from './PostComments'
+import Timestamp from  './Timestamp'
+import DeletePostButton from './DeletePostButton'
+import ViewPostButton from './ViewPostButton'
 
 
 class PostDetail extends Component {
 
-    state = {  }
-
     render() {
 
-        console.log("PostDetail: ", this.props.post1, this.props.selectedPostId1);
+        const { post } = this.props;
+
+        if(!post) return (<div></div>);
+
         return (
             <div>
-                <h1>Post Details Here</h1>
+           
+                    <Panel.Heading><h1>{post.title} <Badge>Score: {post.voteScore}</Badge></h1>
+                        <VoteOnPost post={post}></VoteOnPost>
+                    </Panel.Heading>
+                    <Panel.Body>
+                    <br />
+                    <Row>
+                        <Col md={9}>
+                            <p>{post.body}</p>
+                            <p><small>Written by: <b>{post.author}</b> | <i><Timestamp unixtimestamp={post.timestamp}></Timestamp></i></small></p>
+                            <p><PostComments post={post}></PostComments></p>
+                        </Col>
+
+                        <Col md={1}>
+                            <DeletePostButton post={post}></DeletePostButton>                           
+                        </Col>
+                    </Row>
+                    </Panel.Body>
             </div>
         );
     }
@@ -20,11 +44,11 @@ class PostDetail extends Component {
 
 
 const mapStateToProps = (state, props) => {
-    console.log(" state : ", state);
+
+    const selectedPostId = props.match.params.id
 
     return {
-    post1: state.posts.all.filter((post) => post.id === state.posts.selectedPostId),
-    selectedPostId1: state.posts.selectedPostId
+        post: state.posts.all.length && state.posts.all.filter((post) => post.id === selectedPostId, [])[0]
     }
 }
 
