7da27a720daf2eda69c3629c65c13fc2c6aea63a
Arran Dyer <arran549@gmail.com>
2018-06-09 12:29:10 +0100

    feat: order posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,13 +2,25 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Post from './Post'
 import { Link, withRouter } from 'react-router-dom'
-import { Panel, PageHeader, Well, Button } from 'react-bootstrap'
+import { Panel, PageHeader, Well, Button, DropdownButton, MenuItem, ButtonGroup, FormControl, ControlLabel, FormGroup, ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
 import { selectCategoryActionCreator } from '../actions/navigation.actions'
 import CreatePostButton from './CreatePostButton'
 
 
 
 class Posts extends React.Component {
+
+    constructor() {
+        super()
+        this.sortByDate = this.sortByDate.bind(this);
+        this.sortByScore = this.sortByScore.bind(this);
+        this.orderBy = this.orderBy.bind(this);
+    }
+
+    state = {
+        sortByCategory: 'timestamp',
+        sortByAscending: false
+    }
 
     componentDidMount() {
 
@@ -19,17 +31,60 @@ class Posts extends React.Component {
         }
     }
 
+    sortByScore () {
+        this.setState({sortByCategory: 'voteScore'})
+    }
+
+    sortByDate () {
+        this.setState({sortByCategory: 'timestamp'})
+    }
+
+    orderBy (event) {
+        var sortByAscending = event.target.value === 'ascending';
+        this.setState({sortByAscending: sortByAscending})
+        console.log("sort by:", event.target.value);
+    }
+
     onSelectPosts () {
-            this.props.selectCategory('all');
+        this.props.selectCategory('all');
+    }
+
+    orderPosts () {
+        return this.props.posts.sort((a, b) => {
+            //console.log("a: ", a)
+            //console.log("b:", b)
+            if(this.state.sortByAscending){
+                return a[this.state.sortByCategory] === b[this.state.sortByCategory] ? 0 :  a[this.state.sortByCategory] < b[this.state.sortByCategory] ? -1 : 1
+            }
+            else{
+                return a[this.state.sortByCategory] === b[this.state.sortByCategory] ? 0 :  a[this.state.sortByCategory] > b[this.state.sortByCategory] ? -1 : 1
+            }
+            
+        })
     }
 
     render() {
 
-        const { posts } = this.props
+        const posts = this.orderPosts()
 
         return (
             <div>
-                <PageHeader>Posts</PageHeader>
+                <PageHeader>
+                    Posts {this.state.sortByCategory}
+                    
+                    <FormGroup controlId="orderDirection" className="pull-right">
+                        <FormControl componentClass="select" onChange={this.orderBy}>
+                            <option value="ascending">Ascending</option>
+                            <option value="descending">Descending</option>
+                        </FormControl>
+                    </FormGroup>
+
+                    <ToggleButtonGroup type="radio" name="category" defaultValue="voteScore" className="pull-right" title="Sort By" key="1" id="1">
+                        <ToggleButton value="voteScore" onClick={this.sortByScore}>Score</ToggleButton>
+                        <ToggleButton value="timestamp" onClick={this.sortByDate}>Date</ToggleButton>
+                    </ToggleButtonGroup>
+                    
+                </PageHeader>
                 <Panel>
                     <Panel.Heading>
                         <Panel.Title componentClass="h1">{this.props.selectedCategory} posts</Panel.Title>
