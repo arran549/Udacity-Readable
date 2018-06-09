417b216b0e9144bace4ffabc4f252510f34fbcd5
Arran Dyer <arran549@gmail.com>
2018-06-09 12:30:03 +0100

    feat: show year in timestamp

--- src/components/Timestamp.js
+++ src/components/Timestamp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Moment from 'react-moment'
 
-const Timestamp = ({unixtimestamp}) => <Moment date={new Date(unixtimestamp)} format="ddd DD MMM @ HH:mm"></Moment>
+const Timestamp = ({unixtimestamp}) => <Moment date={new Date(unixtimestamp)} format="ddd DD MMM YYYY @ HH:mm"></Moment>
     
 export default Timestamp;
